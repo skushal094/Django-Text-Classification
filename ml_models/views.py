d31642ef8486482b3c6d308cd31659c3497c43f0
Kushal Shah <skushal094@gmail.com>
2020-05-05 18:21:33 +0530

    Made changes so that result returns each category with probability

--- ml_models/views.py
+++ ml_models/views.py
@@ -11,5 +11,6 @@ def random_forest(request):
         model_pickle_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'trained_models', 'RandomForest.pickle')
         with open(model_pickle_path, "rb") as f:
             model = pickle.load(f)
-        category = model.predict([news,])[0]
+        # category = model.predict([news,])[0]
+        category = dict(zip(model.classes_, model.predict_proba([news,])[0]))
         return render(request, 'ml_models/text_form.html', {'news': news, 'result': category})
